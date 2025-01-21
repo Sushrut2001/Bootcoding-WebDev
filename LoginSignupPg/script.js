021e51666ad05df308447ca80f9c83c71421bde9
Sushrut2001 <sushrutsilekar2001@gmail.com>
2025-01-21 13:02:25 +0530

    Today's Update

---
+++ LoginSignupPg/script.js
@@ -0,0 +1,13 @@
+let signupBtn = document.getElementById('signupBtn');
+let loginBtn = document.getElementById('loginBtn');
+let nameFld = document.getElemrntById('nameFld');
+let sBtn = document.getElementById("sBtn");
+let lBtn = document.getElementById("lBtn");
+
+ loginBtn.addEventListener('click',()=>{
+   sBtn.classList.add('disable');
+   lBtn.classList.remove('disable')
+ })
+
+ console.log(h1);
+ 
